b1f3916e2740722e8eb8a7ba98d7a13069f2cfc8
Blaise A <frost01@gmail.com>
2018-06-03 19:46:13 -0700

    cleaned file

--- app.js
+++ app.js
@@ -13,22 +13,6 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 
-const store = ConfigreStore();
-console.log(store.getState());
-
-store.dispatch(addExpense({description:"Water Bill", amount:23}));
-store.dispatch(addExpense({description:"Gas Bill", amount:79, createdAt:125}));
-store.dispatch(addExpense({description:"Rent", amount:1500}));
-// store.dispatch(setTextFilter('Water'));
-
-// setTimeout( ()=> {
-//     store.dispatch(setTextFilter('bill'));
-// }, 3000);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 // provider: react component that allows consumers to be subsribe to the value's (store) change
 const jsx = (
     <Provider store ={store}>
@@ -37,22 +21,6 @@ const jsx = (
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
-
-const demoState = {
-    expenses: [{
-        id: 'someId',
-        description: 'Jan rent',
-        note: 'This is the final payment for that address',
-        amount:54500,
-        createdAt : 0
-    }],
-    filters: {
-        text:'rent',
-        sortBy: 'amount', // date or amount
-        startDate: undefined,
-        endDate: undefined
-    }
-};
 
 
 
